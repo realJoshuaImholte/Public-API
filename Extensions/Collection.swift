2dccc0b55209aa743c52ba6bf05459e7a3a921f5
Joshua Imhotle <74210958+realJoshuaImholte@users.noreply.github.com>
2023-05-07 16:10:27 -0700

    Delete Collection.swift
    Unneeded

--- Extensions/Collection.swift
+++
@@ -1,6 +0,0 @@
-//This eliminates the risk of IndexOutOfRange Error for UITableView Population. We can't catch en error type when looping through the UITableView
-extension Collection where Indices.Iterator.Element == Index {
-    subscript (exist index: Index) -> Iterator.Element? {
-        return indices.contains(index) ? self[index] : nil
-    }
-}
